419297e1d19ac0a2dfe459e547fa15d75088f572
vaishali91 <vaishali.mangukiya91@gmail.com>
2013-10-24 22:53:39 +0530

    change class name of module

--- application/controllers/student_attendance.php
+++ application/controllers/student_attendance.php
@@ -2,7 +2,7 @@
 if (!defined('BASEPATH'))
 	exit('No direct script access allowed');
 
-class Branch_Manager extends CI_Controller {
+class student_attendance extends CI_Controller {
 
 	public function index() {
 		$this -> load -> view('backend/master_page/top');
