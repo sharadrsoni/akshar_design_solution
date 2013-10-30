f958ef1aec576fa551e78501d0a988ba6676e0cc
Dipesh <dipesh_kakadiiya@yahoo.in>
2013-10-30 05:51:18 +0530

    Test & marks design added

--- application/views/backend/branch_manager/test.php
+++ application/views/backend/branch_manager/test.php
@@ -1,4 +1,4 @@
-<!-- by dips -->
+<!-- START Template Main Content -->
 <section id="main">
 	<!-- START Bootstrap Navbar -->
 	<div class="navbar navbar-static-top">
@@ -9,7 +9,7 @@
 					<a href="#">Dashboard</a><span class="divider"></span>
 				</li>
 				<li class="active">
-					Marks
+					Test
 				</li>
 			</ul>
 			<!--/ Breadcrumb -->
@@ -24,35 +24,35 @@
 			<!-- START Page/Section header -->
 			<div class="span12">
 				<div class="page-header line1">
-					<h4>Marks <small>Maintain marks details over here.</small></h4>
+					<h4>Test <small>Add Test for Batch</small></h4>
 				</div>
 			</div>
 			<!--/ END Page/Section header -->
 		</div>
 		<!--/ END Row -->
 		<!--Page Content Here  -->
-		<div id="Marks">
-
+		<div id="Test">
 			<!-- START Row -->
 			<div class="row-fluid">
 				<!-- Start Tabs -->
 				<div class="tabbable" style="margin-bottom: 25px;">
 					<ul class="nav nav-tabs">
 						<li class="active">
-							<a href="#tab1" data-toggle="tab"><span class="icon icone-eraser"></span>Tests</a>
+							<a href="#tab1" data-toggle="tab"><span class="icon icone-eraser"></span>Branches</a>
 						</li>
 						<li class="">
-							<a href="#tab2" data-toggle="tab"><span class="icon icone-pencil"></span> Add Marks</a>
+							<a href="#tab2" data-toggle="tab"><span class="icon icone-pencil"></span> Add Branch</a>
 						</li>
 					</ul>
 					<div class="tab-content">
 						<div class="tab-pane active" id="tab1">
 							<div class="body-inner">
 								<div class="portlet-body">
-									<table class="table table-striped table-bordered table-hover" id="tblmarks">
+									<table class="table table-striped table-bordered table-hover" id="tbltest">
 										<thead>
 											<tr>
 												<th>Test ID</th>
+												<th class="hidden-480">Branch Name</th>
 												<th class="hidden-480">Test Date</th>
 												<th class="hidden-480">Maximum Marks</th>
 												<th >Action</th>
@@ -64,11 +64,7 @@
 												<td>33/33/3333</td>
 												<td>XXX</td>
 												<td>
-												<ul class="nav nav-tabs nav-stacked pull-right">
-													<li>
-														<a role="button" data-toggle="modal" href="#form_marks"><span class="icon icone-home"></span> Add Marks of Student</a>
-													</li>
-												</ul></td>
+												/td>
 											</tr>
 										</tbody>
 									</table>
@@ -76,7 +72,7 @@
 							</div>
 						</div>
 						<div class="tab-pane" id="tab2">
-							<form class="form-horizontal span12 widget shadowed yellow" id="form_mark">
+							<form class="form-horizontal span12 widget shadowed yellow" id="form_test">
 								<div class="alert alert-error hide">
 									<button class="close" data-dismiss="alert"></button>
 									You have some form errors. Please check below.
@@ -87,22 +83,13 @@
 								</div>
 
 								<div class="body-inner">
-									<div class="alert alert-error hide">
-										<button class="close" data-dismiss="alert"></button>
-										You have some form errors. Please check below.
-									</div>
-									<div class="alert alert-success hide">
-										<button class="close" data-dismiss="alert"></button>
-										Your form validation is successful!
-									</div>
-
-									<div class="body-inner">
-										<h3 class="form-section">Batch Info</h3>
-
-										<div class="control-group">
-											<label class="control-label">Batch ID<span class="required">*</span></label>
-											<div class="controls">
-												<select class="span4" name="batch_id" id="batch_id">
+									<h3 class="form-section">Batch Info</h3>
+									<!-- Batch ID -->
+									<div class="control-group">
+										<label class="control-label">Batch ID<span class="required">*</span></label>
+										<div class="controls">
+											<div class="span4">
+												<select class="span12" name="batch_id" id="batch_id">
 													<option value="">Select...</option>
 													<option value="Category 1">Category 1</option>
 													<option value="Category 2">Category 2</option>
@@ -111,39 +98,34 @@
 												</select>
 											</div>
 										</div>
-										<!--/ Course -->
-										<h3 class="form-section">Student Info</h3>
-										<!-- LIst -->
-										<div class="control-group">
-											<table class="table table-striped table-bordered table-hover" id="obtainmarks">
-												<thead>
-													<tr>
-													<tr>
-														<th>Student ID</th>
-														<th class="hidden-480">Obtained Marks </th>
-													</tr>
-												</thead>
-
-												<tbody id="lst_batch_timing">
-													<tr class="odd gradeX">
-														<td> Student_ID XXX </td>
-														<td>
-														<input type="text" name="obtained_marks" id="obtained_marks" class="span2"/>
-														</td>
-													</tr>
-												</tbody>
-											</table>
-										</div><!--/ List-->
-										<!-- Form Action -->
-										<div class="form-actions">
-											<button type="submit" class="btn btn-primary">
-												Register
-											</button>
-											<button type="button" class="btn">
-												Cancel
-											</button>
-										</div><!--/ Form Action -->
+									</div><!--/ Batch ID -->
+									<h3 class="form-section">Test Info</h3>
+									<!-- Start Date -->
+									<div class="control-group">
+										<label class="control-label">Test Date<span class="required">*</span></label>
+										<div class="controls">
+											<div class="input-append span6" id="test_date_datepicker">
+												<input type="text" name="test_date" id="test_date" class="m-wrap span7">
+												<span class="add-on"><i class="icon-calendar"></i></span>
+											</div>
+										</div>
+									</div><!--/ Start Date -->
+									<div class="control-group">
+										<label class="control-label">Test Marks</label>
+										<div class="controls">
+											<input type="text" name="test_marks" id="test_marks" class="span8">
+										</div>
 									</div>
+									<!-- Form Action -->
+									<div class="form-actions">
+										<button type="submit" class="btn btn-primary">
+											Register
+										</button>
+										<button type="button" class="btn">
+											Cancel
+										</button>
+									</div><!--/ Form Action -->
+								</div>
 							</form>
 						</div>
 					</div>
@@ -156,3 +138,4 @@
 	</div>
 	<!--/ END Content -->
 </section>
+<!--/ END Template Main Content -->
