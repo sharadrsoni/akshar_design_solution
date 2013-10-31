ee576d07228d68086c88d7f44d6fed2cb2ca64b6
UnnatiPatel <unnati2792@gmail.com>
2013-10-31 16:04:50 +0530

    eventtype function added in branchmanager.php file

--- application/controllers/branch_manager.php
+++ application/controllers/branch_manager.php
@@ -466,6 +466,17 @@ class Branch_Manager extends CI_Controller {
 		$this -> load -> view('backend/js/changepassword_js');
 		$this -> load -> view('backend/master_page/bottom');
 	}
+	
+	public function eventtype() {
+		$data['title'] = "ADS | Event Type";
+		$this -> load -> view('backend/master_page/top', $data);
+		$this -> load -> view('backend/css/eventtype_css');
+		$this -> load -> view('backend/master_page/header');
+		$this -> load -> view('backend/branch_manager/eventtype');
+		$this -> load -> view('backend/master_page/footer');
+		$this -> load -> view('backend/js/eventtype_js');
+		$this -> load -> view('backend/master_page/bottom');
+	}
 
 	public function delete_event($eventId) {
 		$this -> load -> model('event_model');
