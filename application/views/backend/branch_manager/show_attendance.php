22e8af9945bad2bd3ce3077bc82e6e979f5d2e5b
Dipesh <dipesh_kakadiiya@yahoo.in>
2013-11-03 03:43:15 +0530

    issue calender css not render Solver

--- application/views/backend/branch_manager/show_attendance.php
+++ application/views/backend/branch_manager/show_attendance.php
@@ -9,17 +9,10 @@
 					<a href="#">Components</a><span class="divider"></span>
 				</li>
 				<li class="active">
-					Calendar
+					Attendance
 				</li>
 			</ul>
 			<!--/ Breadcrumb -->
-
-			<!-- Daterange Picker -->
-			<div id="reportrange" class="pull-right hidden-phone">
-				<span class="icon icon-calendar"></span>
-				<span id="rangedate">June 7, 2013 - June 13, 2013</span><span class="caret"></span>
-			</div>
-			<!--/ Daterange Picker -->
 		</div>
 	</div>
 	<!--/ END Bootstrap Navbar -->
@@ -31,7 +24,7 @@
 			<!-- START Page/Section header -->
 			<div class="span12">
 				<div class="page-header line1">
-					<h4>Calendar <small>organizing days for social, religious, commercial, or administrative purposes.</small></h4>
+					<h4>Attendance <small>Show your attendaace details here.</small></h4>
 				</div>
 			</div>
 			<!--/ END Page/Section header -->
