4ab6cddf525c9b722c0798e4faaf2cbb406fd5ce
Sharad Soni <sharadrsoni@gmail.com>
2013-11-12 22:49:30 +0530

    Errors in course_model

--- application/models/course_model.php
+++ application/models/course_model.php
@@ -2,47 +2,43 @@
 if (!defined('BASEPATH'))
 	exit('No direct script access allowed');
 /**
- * 
+ *
  */
 class course_model extends CI_Model {
-	
+
 	public function getDetailsOfCourse() {
 
 		$this -> db -> from('course');
 		return $this -> db -> get() -> result();
 
 	}
-	
-	public function getAllDetails()
-	{
-		return $this->db->get("course")->result();
+
+	public function getAllDetails() {
+		return $this -> db -> get("course") -> result();
 	}
-<<<<<<< HEAD
-	
-		public function addCourse($data) {
+
+	public function addCourse($data) {
 		if (isset($data)) {
-			die (print_r($data));
+			die(print_r($data));
 			return $this -> db -> insert('course', $data);
 		} else {
 			return false;
 		}
 	}
-		
-		public function deleteCourse($courseCode) {
+
+	public function deleteCourse($courseCode) {
 		if (isset($courseCode)) {
 			$this -> db -> where('courseCode', $courseCode);
 			$this -> db -> delete('course');
 		}
 
 	}
-=======
 
 	public function getCourseName($courseCode) {
 		$this -> db -> where("course.courseCode", $courseCode);
 		$this -> db -> from('course');
 		return $this -> db -> get() -> result();
 	}
-		
->>>>>>> 8aec57050ce01e0425c2aab56edc34532250cf16
+
 }
 ?>
