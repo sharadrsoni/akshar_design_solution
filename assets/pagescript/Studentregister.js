e38a3195027209190c59b30cb4faf8d5f2e170ed
Dipesh <dipesh_kakadiiya@yahoo.in>
2013-11-12 23:13:36 +0530

    Student Registration JS updated

--- assets/pagescript/Studentregister.js
+++ assets/pagescript/Studentregister.js
@@ -113,37 +113,41 @@ var StudentRegistration = function() {
 	return {
 		//main function to initiate the module
 		init_formvalidation : function() {
-
 			FormValidationStudentRegistration();
 			FormValidationCourseRegistration();
-
+		},
+		init_uijquery : function() {
+			$("#courseid").change(function() {
+				$("#batchid").children().remove();
+				$('#batchid').html("<option value=\"\">Select...</option>");
+				$.ajax({
+					url : "../ajax_manager/batch/" + $("#courseid").val(),
+					dataType : 'json',
+					async : true,
+					success : function(json) {
+						if (json) {
+							$.each(json.batch_list, function(i, item) {
+								$('#batchid').append("<option value=" + item.batchId + ">" + item.batchStartDate + "</option>");
+							});
+						}
+					}
+				});
+			});
+			$("#studentid").change(function() {
+				$.ajax({
+					url : "../ajax_manager/studentBatch/" + $("#studentid").val(),
+					dataType : 'json',
+					async : true,
+					success : function(json) {
+						if (json) {
+							$.each(json.batch_list, function(i, item) {
+								$('#lst_Courses').append("<tr class='odd gradeX'><td>" + $("#weekday option[value='" + item.batchTimingWeekday + "']").text() + "<input type='hidden' name='batch_timing[]' value='" + item.batchTimingWeekday + "'/></td><td class='hidden-480'>" + item.batchTimingStartTime + "<input type='hidden' name='batch_timing[]' value='" + item.batchTimingStartTime + "'/></td><td class='hidden-480'>" + item.batchTimingEndTime + "<input type='hidden' name='batch_timing[]' value='" + item.batchTimingEndTime + "'/></td><td><a onclick='removebatchtime(this)' class='btn red icn-only'><i class='icon-remove icon-white'></i></a></td></tr>");
+							});
+						}
+					}
+				});
+			});
 		}
 	};
 }();
-
-function viewRegisterCourses(studentid) {
-	$.ajax({
-		url : "../ajax_manager/studentBatch/" + $("#studentid").val(),
-		dataType : 'json',
-		//data : "director=" + argdirector,
-		async : true,
-		success : function(json) {
-			if (json) {
-				$("#course_id").val(json.batch_list[0].courseCode);
-				$("#faculty_id").val(json.batch_list[0].facultyId);
-				$("#start_date").val(json.batch_list[0].batchStartDate);
-				$("#duration").val(json.batch_list[0].batchDuration);
-				$("#strength").val(json.batch_list[0].batchStrength);
-				$('#lst_batch_timing').html("");
-				$.each(json.weekdays, function(i, item) {
-					$('#lst_batch_timing').append("<tr class='odd gradeX'><td>" + $("#weekday option[value='"+item.batchTimingWeekday+"']").text()+ "<input type='hidden' name='batch_timing[]' value='" + item.batchTimingWeekday + "'/></td><td class='hidden-480'>" + item.batchTimingStartTime + "<input type='hidden' name='batch_timing[]' value='" + item.batchTimingStartTime + "'/></td><td class='hidden-480'>" + item.batchTimingEndTime + "<input type='hidden' name='batch_timing[]' value='" + item.batchTimingEndTime + "'/></td><td><a onclick='removebatchtime(this)' class='btn red icn-only'><i class='icon-remove icon-white'></i></a></td></tr>");
-				});
-				$("#tablink1").parent().removeClass("active");
-				$("#tab1").removeClass("active");
-				$("#tab2").addClass("active");
-				$("#batchId").val(batchid);
-			}
-		}
-	});
-}
 
